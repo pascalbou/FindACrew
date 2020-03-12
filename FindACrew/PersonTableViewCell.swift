6273c004e10c3bd39c555f09d87b82fd0a19cfe3
krikaz <pascal.bou@protonmail.com>
2020-03-12 19:11:50 +0100

    Set up UI in storyboard, wired up interface, tested with a single hardcoded cell

---
+++ FindACrew/PersonTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonTableViewCell.swift
+//  FindACrew
+//
+//  Created by krikaz on 3/12/20.
+//  Copyright © 2020 thewire. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var birthYearLabel: UILabel!
+    
+}
